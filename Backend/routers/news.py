a40f1e86ee6c12c04f1fee2294272d46d350baf2
mon2384 <152243939+bart2559b@users.noreply.github.com>
2024-10-13 19:35:33 +0700

    get all

--- Backend/routers/news.py
+++ Backend/routers/news.py
@@ -5,10 +5,10 @@ from models.user import User
 from models.typer_user import RoleEnum
 from crud.typer_user import get_typer_user_by_id
 from utils.auth import get_current_user
-from schemas.news import NewsCreatedResponse, NewsCreate, NewsUpdate, NewsUpdateResponse
+from schemas.news import NewsCreatedResponse, NewsCreate, NewsUpdate, NewsUpdateResponse, NewsResponse
 from database import get_db
 from models.news import News
-from crud.news import create_news, update_news
+from crud.news import create_news, update_news, getAll
 
 router = APIRouter()
 
@@ -38,6 +38,26 @@ def get_editor_user(db: Session, current_user: dict) -> Optional[User]:
 
     return user_id
 
+def get_viewer_user(db: Session, current_user: dict) -> Optional[User]:
+    user_id = current_user.get("sub")
+    typer_user = get_typer_user_by_id(db, user_id)
+
+    if not typer_user or typer_user.role != RoleEnum.writer:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You are not authorized to view news.",
+        )
+
+    return user_id
+
+@router.get('/', response_model=list[NewsResponse])
+def get_all_news_item(
+    db: Session = Depends(get_db),
+    current_user: dict = Depends(get_current_user)
+):
+    get_viewer_user(db, current_user)
+    news_all = getAll(db)
+    return news_all
 
 @router.post('/', response_model=NewsCreatedResponse, status_code=status.HTTP_201_CREATED)
 def create_news_item(
