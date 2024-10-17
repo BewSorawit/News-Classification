56345a5f69d607c254676d34f38dccac489b8eaf
KONG_jirayu <jirayu.nookit.th@gmail.com>
2024-10-17 12:32:33 +0700

    chage path login

--- news-classification-app/resources/views/test-login.blade.php
+++ news-classification-app/resources/views/test-login.blade.php
@@ -63,7 +63,7 @@
                                             window.location.href = '/user'; // ถ้าเป็น admin เรียกหน้า user
                                         } else if (roleData.role !== 'Admin') {
                                             // window.location.href = '/news'; // ถ้าไม่ใช่ admin เรียกหน้า news
-                                            location.replace('/home');
+                                            location.replace('/news');
                                         }
 
                                 },
